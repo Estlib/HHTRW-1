47da252af9aed0f90a9192890a87e113778ad4ab
Estlib <75094683+Estlib@users.noreply.github.com>
2023-03-17 12:00:37 +0200

    Add project files.

---
+++ Plat2d-2/EngineCore/EngineCore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plat2d_2.EngineCore
+{
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+    public abstract class EngineCore
+    {
+        private Vector2 ScreenSize = new Vector2(512, 512);
+        private string Title;
+        private Canvas Windows = null;
+
+        public EngineCore(Vector2 ScreenSize, string Title)
+        {
+            this.ScreenSize = ScreenSize;
+            this.Title = Title;
+
+
+        }
+    }
+}
---
+++ Plat2d-2/EngineCore/Vector2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plat2d_2.EngineCore
+{
+    public class Vector2
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Vector2()
+        {
+            X = Zero().X;
+            Y = Zero().Y;
+        }
+        public Vector2(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+            //Vector2.Zero();
+        }
+        /// <summary>
+        /// Returns x and y as zero
+        /// </summary>
+        /// <returns></returns>
+        public static Vector2 Zero()
+        {
+            return new Vector2(0, 0);
+        }
+    }
+}
