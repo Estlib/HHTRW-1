f26de1a01137cdeae0675c190ad664f4719d056c
Estlib <75094683+Estlib@users.noreply.github.com>
2025-08-20 22:25:47 +0300

    Started work on a simple ogg-based sfx engine. will flesh it out once completed into a full audio system

---
+++ Plat2d-2/EngineCore/SFXEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plat2d_2.EngineCore
+{
+    public class SFXEngine : SoundPlayer
+    {
+        public static SoundPlayer Coin;
+        public bool isCoinPlaying { get; set; }
+
+        public static SoundPlayer Bullet;
+        public bool isBulletPlaying { get; set; }
+
+
+    }
+
+
+}
