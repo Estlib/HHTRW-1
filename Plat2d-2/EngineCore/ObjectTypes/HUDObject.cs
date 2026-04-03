98fb207c80c7c0d2f3f38d58e70ea263a5a46256
Estlib <75094683+Estlib@users.noreply.github.com>
2026-04-03 18:50:29 +0300

    the hud follows, work ongoing on the dynamic hud elements
    made some sprites

---
+++ Plat2d-2/EngineCore/ObjectTypes/HUDObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plat2d_2.EngineCore.ObjectTypes
+{
+    enum HudElementType
+    {
+        Digit, Bar, Icon, Name
+    }
+    enum BarElementFillAmount
+    {
+       Full, Half, None
+    }
+    internal class HUDObject
+    {
+        //shared properties
+        public Sprite2d Display { get; set; }
+        public List<Bitmap> DisplayElements { get; set; }
+
+        //digit item properties
+        public HudElementType ElementDigitType { get; set; } = HudElementType.Digit;
+        public int DisplayedDataInt { get; set; }
+
+        //bar item properties
+        public HudElementType ElementBarType { get; set; } = HudElementType.Bar;
+        public BarElementFillAmount ElementFillAmount { get; set; } = BarElementFillAmount.None;
+
+
+    }
+}
