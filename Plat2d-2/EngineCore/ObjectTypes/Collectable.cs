fa527969e0a57ccc28cffa617a921fc28d73ab15
Estlib <75094683+Estlib@users.noreply.github.com>
2026-07-05 23:10:29 +0300

    created collectable, am trying to add one template health item. Constructor is screaming at me.
    yes .-.

---
+++ Plat2d-2/EngineCore/ObjectTypes/Collectable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plat2d_2.EngineCore.ObjectTypes
+{
+    public enum ItemType
+    {
+        Undefined,
+        Trigger,
+        Ammo,
+        Health,
+        Life,
+        WeaponUnlock,
+        StoryItem
+    }
+    public class Collectable
+    {
+        public Sprite2d Sprite { get; set; }
+        public List<Bitmap> AniFrames { get; set; }
+        public int Value { get; set; } = 0;
+        public ItemType WhatThisType { get; set; } = ItemType.Undefined;
+        public bool IsHidden { get; set; } = true;
+
+        /// <summary>
+        /// Templatetype
+        /// </summary>
+        public Collectable(Sprite2d mainimage, List<Bitmap> frames, int amount, ItemType type )
+        {
+            Sprite = mainimage;
+            AniFrames = frames;
+            Value = amount;
+            WhatThisType = type;
+        }
+    }
+}
